b2d4918d2d87fbbd4cff7877f2c844f666f9433c
Dejan85 <xypnotica@gmail.com>
2021-05-12 12:49:54 +0200

    created footer, header, layout

--- components/frontend/layout/Layout.tsx
+++ components/frontend/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import { Header, Footer } from "components/frontend";
 
 const Layout = ({ children, title = "Proenglish" }) => {
   return (
@@ -9,7 +10,9 @@ const Layout = ({ children, title = "Proenglish" }) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+      <Header />
       {children}
+      <Footer />
     </div>
   );
 };
