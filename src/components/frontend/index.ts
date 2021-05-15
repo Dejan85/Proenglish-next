1e4b23f256ffe84842679d2d8fe7da124b93b423
Dejan85 <xypnotica@gmail.com>
2021-05-15 05:10:09 +0200

    pricelist

--- src/components/frontend/index.ts
+++ src/components/frontend/index.ts
@@ -3,4 +3,5 @@ export { Header } from "./header";
 export { About } from "./about";
 export { Course } from "./course";
 export { Exams } from "./exams";
+export { Pricelist } from "./pricelist";
 export { Footer } from "./footer";
