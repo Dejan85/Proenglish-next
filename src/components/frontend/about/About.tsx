bb980d814fec4db91d8225ec8aba0a2229b6ea8b
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-06-13 22:00:07 +0200

    added dynamic title custom hook

--- src/components/frontend/about/About.tsx
+++ src/components/frontend/about/About.tsx
@@ -7,15 +7,18 @@ import { ABOUT_SCOPE } from "./constants";
 import { fetchAboutDataAction, reducer } from "./slice";
 import saga from "./saga";
 import RenderUi from "./partials/UiRender";
+import useTitle from "src/hooks/title/useTitle";
 
 const About = () => {
   useInjectReducer({ key: ABOUT_SCOPE, reducer });
   useInjectSaga({ key: ABOUT_SCOPE, saga });
   const dispatch = useDispatch();
   const { aboutData } = useSelector(getAboutData);
+  const { setTitleHandler } = useTitle();
 
   useEffect(() => {
     dispatch(fetchAboutDataAction());
+    setTitleHandler("About Title");
   }, []);
 
   return <RenderUi aboutData={aboutData} />;
--- src/components/frontend/layout/Layout.tsx
+++ src/components/frontend/layout/Layout.tsx
@@ -2,8 +2,11 @@ import React, { useEffect } from "react";
 import Head from "next/head";
 import { Header, Footer } from "src/components/frontend";
 import { Container } from "src/components/ui";
+import useTitle from "src/hooks/title/useTitle";
 
-const Layout = ({ children, title = "Proenglish" }) => {
+const Layout = ({ children }) => {
+  const { pageTitle } = useTitle();
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -11,7 +14,7 @@ const Layout = ({ children, title = "Proenglish" }) => {
   return (
     <Container>
       <Head>
-        <title>{title}</title>
+        <title>{pageTitle}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
